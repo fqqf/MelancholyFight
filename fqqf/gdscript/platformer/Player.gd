76d38010bf7eb6674579f9d000916ddf3f412352
fqqf <77983090+fqqf@users.noreply.github.com>
2021-09-12 23:09:28 +0300

    Add tilemap sprites

--- fqqf/gdscript/platformer/Player.gd
+++ fqqf/gdscript/platformer/Player.gd
@@ -16,12 +16,16 @@ onready var sprite = $Sprite
 onready var animation_player = $AnimationPlayer
 
 var motion = Vector2.ZERO
+var snap_vector = Vector2.ZERO
+var has_jumped = false
 
 func _physics_process(delta):
+	has_jumped = false
 	var input_vector = get_input_vector()
 	
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector)
+	update_snap_vector()
 	jump_check()
 	apply_gravity(delta)
 	update_animations(input_vector)
@@ -49,19 +53,42 @@ func get_input_vector() -> Vector2:
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	return input_vector
+	
+func update_snap_vector():
+	if is_on_floor():
+		snap_vector = Vector2.DOWN
 
 func jump_check():
 	if is_on_floor(): # Works only, if there is a force that constantly pushes you down
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
+			has_jumped = true
+			snap_vector = Vector2.ZERO
 	elif Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2: # Doesnt let you to get more force, if you have more than JUMP_FORCE/2
 		motion.y = -JUMP_FORCE/2 # You need to remember that going down is actually -y
 			
 func apply_gravity(delta):
-#	if not is_on_floor():
+	#if not is_on_floor(): # Now that matters
 		motion.y += GRAVITY * delta
 		motion.y = min(motion.y, JUMP_FORCE) # Minimum value
 
 func move():
-	motion = move_and_slide(motion, Vector2.UP)
+	var is_flying = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_position = position
+	var last_motion = motion
 	
+	motion = move_and_slide_with_snap(motion, snap_vector * 4, Vector2.UP,true,4,deg2rad(MAX_SLOPE_ANGLE))
+	
+	# Landing
+	if is_flying and is_on_floor():
+		motion.x = last_motion.x
+	
+	# Just left the ground
+	if was_on_floor and not is_on_floor() and not has_jumped:
+		motion.y = 0
+		position.y = last_position.y
+
+	# Prevent sliding (If our motion is tiny, stops)
+	if is_on_floor() and abs(motion.x) < 3:
+		position.x = last_position.x
