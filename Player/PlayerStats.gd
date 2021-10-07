a362c73d906fe23f013cbb3ae51a022ebf573f27
Rubatskiy <79303359+Rubatskiy@users.noreply.github.com>
2021-10-08 00:14:16 +0300

    43 is over

--- Player/PlayerStats.gd
+++ Player/PlayerStats.gd
@@ -6,6 +6,7 @@ class_name PlayerStats
 var max_health = 4
 var health = max_health setget set_health
 
+signal player_health_change(value)
 signal player_died
 
 
@@ -14,6 +15,7 @@ func set_health(value):
 	if value < health:
 		Events.emit_signal("add_screenshake", 0.8, 0.5)
 	health = clamp(value, 0, max_health)
+	emit_signal("player_health_change", health)
 	if health == 0:
 		emit_signal("player_died")
 		
