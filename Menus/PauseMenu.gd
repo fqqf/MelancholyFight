b86f61d01c9f64f57a7c329083062462555fab77
Rubatskiy <79303359+Rubatskiy@users.noreply.github.com>
2021-10-10 17:04:25 +0300

    46pause menu over

---
+++ Menus/PauseMenu.gd
@@ -0,0 +1,22 @@
+extends ColorRect
+
+var paused = false setget set_paused
+
+func set_paused(value):
+	paused = value
+	get_tree().paused = paused
+	visible = paused
+	
+
+# warning-ignore:unused_argument
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		self.paused = !paused
+		
+
+func _on_ResumeButton_pressed():
+	self.paused = false
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
--- UI/HealthMeter.gd
+++ UI/HealthMeter.gd
@@ -8,6 +8,6 @@ func _ready():
 	PlayerStats.connect("player_health_change", self, "_on_player_health_changed")
 	
 func _on_player_health_changed(value):
-	full.rect_size.x = 	value * 5 + 1 
+	full.rect_size.x = value * 5 + 1 
 	
 	
