2144402d4fc4e72a6f6df2ff5dd8b317642203ff
fqqf <77983090+fqqf@users.noreply.github.com>
2021-11-09 18:46:44 +0300

    Fix wrong CameraViewport moving

--- Cave-Game/core/Config.gd
+++ Cave-Game/core/Config.gd
@@ -1,7 +1,10 @@
 extends Node
 
 const logging_string = "[{TIME}][{MOD}][{LVL}] :: {MSG}"
-const DEFAULT_ASPECT_RATIO = 500/500
+var DEFAULT_ASPECT_RATIO = float(ProjectSettings.get_setting("display/window/size/width"))/float(ProjectSettings.get_setting("display/window/size/height"))
+var DEFAULT_WIDTH = ProjectSettings.get_setting("display/window/size/width")+0.0
+var DEFAULT_HEIGHT = ProjectSettings.get_setting("display/window/size/height")+0.0
 
 func _ready():
 	Logger.output_format = logging_string
+	Logger.log("PROJECT WIDTH : "+str(DEFAULT_WIDTH)+", PROJECT HEIGHT : "+str(DEFAULT_HEIGHT)+", ASPECT : "+str(DEFAULT_ASPECT_RATIO))
--- Cave-Game/core/Main.gd
+++ Cave-Game/core/Main.gd
@@ -1,33 +1,25 @@
 extends Node2D
 
 var viewport
-onready var camera = $Camera
+onready var cameraViewport = $CameraViewport
+onready var camera = $CameraViewport/Camera
 
-var lastPos 
-var curPos
+var last_pos 
+var cur_pos
 
 func _ready():
 	Singleton.main = self
 	viewport = get_viewport()
 	viewport.connect("size_changed", self, "_root_viewport_size_changed")
-	lastPos = camera.position.x
-	curPos = camera.position.x
+	last_pos = cameraViewport.position.x
+	cur_pos = cameraViewport.position.x
+	
 	_root_viewport_size_changed()
+	
+
 
 func _root_viewport_size_changed():
-	var cur_width = viewport.size.x/viewport.size.y
-	var default_width = Config.DEFAULT_ASPECT_RATIO
-	var diff = cur_width/(default_width/cur_width)
-	print(camera.position.x)
-	_adjustPos()
-	camera.position.x = (cur_width/default_width)*curPos
-	lastPos=camera.position.x
-	
-func _process(delta):
-	_adjustPos()
-	
-
-func _adjustPos():
-	if lastPos!=camera.position.x:
-		curPos += lastPos-camera.position.x
-		lastPos=camera.position.x
+	var cur_ratio = viewport.size.x/viewport.size.y
+	var default_ratio = Config.DEFAULT_ASPECT_RATIO
+	var diff = cur_ratio/(default_ratio/cur_ratio)
+	cameraViewport.position.x = (cur_ratio/default_ratio)*(Config.DEFAULT_WIDTH/2)-Config.DEFAULT_WIDTH/2
