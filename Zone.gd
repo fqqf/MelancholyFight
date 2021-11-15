cff76e71a0bb83e1ec9bdd9aa8535d0cca1c17dc
Rubatskiy <79303359+Rubatskiy@users.noreply.github.com>
2021-11-16 02:56:35 +0300

    Create and destroy platforms

--- Zone.gd
+++ Zone.gd
@@ -5,51 +5,66 @@ extends Node2D
 
 const U_BLOCK_SIZE = 16
 
-export var SIZE_LIMITS = [7,58] # Пусть если сейчас нижний, то с большей вероятностью выпадет такого же уровня, или выше
-export var HEIGHT_LIMITS = [55.247, 90.2]#[56.247, 92.3] # TODO: РАСПРЕДЕЛИТЬ ВЕРОЯТНОСТИ (ВСЕГО) 
-export var GAP_LIMITS = [70,170]
+export var SIZE_LIMITS = [3,20]
+export var HEIGHT_LIMITS = [56.247, 92.3]
+export var GAP_LIMITS = [40,100]
 export var BORDER_SIZE = 5*U_BLOCK_SIZE
 
 const platformScene = preload("res://core/gameplay/Platform.tscn")
 
-const CREATION_LENGTH = 300
+
+const CREATION_LENGTH = 2000
 
 onready var Dynamic = $Dynamic
+onready var Souly = $Souly
 
-var _length
+
 var _length_taken = 0
-
-func _ready():
-	init(20000)
-
-func init(length = 100, tag = 0):
-	_length = length
-	fill_terrain_array()
-	print(terrain_array)
-	create_platforms()
-	
 var terrain_array = []
+var count=0
+
+func _process(delta):
+	if count == 0:
+		fill_terrain_array(1,CREATION_LENGTH) #формируем платформы для игрока на уровень смещения
+		create_platforms()
+	else:
+		#привязка к движению + смещение
+		if Dynamic.position.x - CREATION_LENGTH < -_length_taken:
+			fill_terrain_array()
+			create_platforms()
+
+
+
 
 func create_platforms():
 	var platform_instance
-	for platform in terrain_array:
+	var platform
+	while count != terrain_array.size():
+		platform = terrain_array[count]
 		platform_instance = platformScene.instance()
 		platform_instance.build(platform)
 		Dynamic.add_child(platform_instance)
-
-func fill_terrain_array():
+		count+=1
+	
+func fill_terrain_array(quantity=1, _length = 0):
 	var alloc_len = 0
 	var height = 0
-	while true:
-		if _length_taken <= _length-BORDER_SIZE:
+	if _length == 0: #если длинна платформ не задана 
+		while quantity:
+			#1 длинна платформы, 2 - высота, 3 записываем в массив, 4 получаем следующую точку начала платформы 
+			alloc_len = round(rand_range(SIZE_LIMITS[0], SIZE_LIMITS[1]))*U_BLOCK_SIZE
+			height = rand_range(HEIGHT_LIMITS[0],HEIGHT_LIMITS[1])
+			terrain_array.append(Vector3(_length_taken, height, alloc_len/U_BLOCK_SIZE))
+			_length_taken+=alloc_len+rand_range(GAP_LIMITS[0], GAP_LIMITS[1])
+			quantity-=1
+			
+	else:
+		while _length_taken <= _length-BORDER_SIZE:
 			alloc_len = min(round(rand_range(SIZE_LIMITS[0], SIZE_LIMITS[1]))*U_BLOCK_SIZE,_length-_length_taken)
 			height = rand_range(HEIGHT_LIMITS[0],HEIGHT_LIMITS[1])
-			
 			if _length_taken+alloc_len == _length:
 				print("ENDING")
 				alloc_len += BORDER_SIZE
-			
 			terrain_array.append(Vector3(_length_taken, height, alloc_len/U_BLOCK_SIZE)) # x, y (height), width
 			_length_taken+=alloc_len+rand_range(GAP_LIMITS[0], GAP_LIMITS[1])
-		else:
-			break
+		
