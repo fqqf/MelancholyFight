b544595df3dc528e92487a3467626e1b779d0b95
Rubatskiy <79303359+Rubatskiy@users.noreply.github.com>
2021-11-24 22:22:27 +0300

    Add two new function to utils.gd

--- core/Utils.gd
+++ core/Utils.gd
@@ -3,12 +3,11 @@ extends Node
 
 func _ready():
 	randomize()
-	pass # Replace with function body.
-
-#print(Utils.get_prob([-30,1000], [[-30,400,50], [401, 405, 400], [406,900,30],[901,1000,5]]))
 	
 
-func get_prob(all_range, range_percent):
+#secure
+#print(Utils.get_prob_s([-30,1000], [[-30,400,50], [401, 405, 400], [406,900,30],[901,1000,5]]))
+func get_prob_s(all_range, range_percent):
 	var sum_percent =0
 	for percent in range_percent:
 		sum_percent += percent[2]
@@ -18,3 +17,32 @@ func get_prob(all_range, range_percent):
 		if find_percent <=0: 
 			assert( all_range[0] <= find_range[0] and find_range[0] <=all_range[1] and all_range[0] <= find_range[1] and find_range[1] <=all_range[1] )
 			return int(round(rand_range(find_range[0], find_range[1])))
+
+#unsecure
+#print(Utils.get_prob([[-30,400,50], [401, 405, 400], [406,900,30],[901,1000,5]]))
+func get_prob(range_percent):
+	var sum_percent =0
+	for percent in range_percent:
+		sum_percent += percent[2]
+	var find_percent = rand_range(0, sum_percent)
+	for find_range in range_percent:
+		find_percent -= find_range[2]
+		if find_percent <=0: 
+			return int(round(rand_range(find_range[0], find_range[1])))
+
+#percent secure	
+#print(Utils.get_prob_ps([-30,1000], [[10,30,50], [31, 60, 400], [61,85,30],[86,100,5]]))
+func get_prob_ps(all_range, range_percent):
+	var sum_percent =0
+	var one_percent: float
+	for percent in range_percent:
+		sum_percent += percent[2]
+	var find_percent = rand_range(0, sum_percent)
+	for find_range in range_percent:
+		find_percent -= find_range[2]
+		if find_percent <=0:
+			assert( 0 <= find_range[0] and find_range[0] <=100 and 0 <= find_range[1] and find_range[1] <=100 )
+			one_percent = (float(abs(all_range[0])) + abs(all_range[1]))/100
+			var new_range = [all_range[0]+(find_range[0]*one_percent), all_range[0]+(find_range[1]*one_percent)]
+			return int(round(rand_range(new_range[0], new_range[1])))
+
