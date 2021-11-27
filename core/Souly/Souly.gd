d38745b74132c337ac3f3e8d6de3430a1246455f
fqqf <77983090+fqqf@users.noreply.github.com>
2021-11-27 16:52:06 +0300

    Implement moving Souly to her origin pos, when she is away from it

--- core/Souly/Souly.gd
+++ core/Souly/Souly.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export (int) var GRAVITY = 210
 export (int) var JUMP_FORCE = 125
+export (int) var SPEED = 1000
 
 onready var coyote_timer = $CoyoteJumpTimer
 onready var collectables_detector = $CollectablesDetector
@@ -12,16 +13,19 @@ var has_jumped = false
 var jump_released = false
 var early_released = false
 
+var start_position
+
+
 func _ready():
 	collectables_detector.connect("area_entered",get_parent(), "_on_souly_pickup_collectable")
-
+	start_position = position
 func move():
 	var was_in_air = not is_on_floor()
 	var was_on_floor = is_on_floor()
 	var last_motion = motion
 	var last_position = position
 	
-	motion = move_and_slide(motion, Vector2.UP) 
+	motion = move_and_slide_with_snap(motion, Vector2.DOWN, Vector2.UP) 
 	
 	if was_on_floor and not is_on_floor() and not has_jumped:
 		motion.y = 0
@@ -29,14 +33,30 @@ func move():
 		coyote_timer.start()
 		#Logger.log("Coyote timer has started")
 		
-	if was_in_air and is_on_floor():
-		motion.x = last_motion.x
+	#if was_in_air and is_on_floor():
+	#	motion.x = last_motion.x
+
+var is_adjustin_pos
 
 func _physics_process(delta):
 	has_jumped = false
 	apply_gravity(delta)
 	jump_check()
+	adjust_pos_to_start_pos(delta)
 	move()
+	
+func adjust_pos_to_start_pos(delta):
+	if  abs(start_position.x-position.x)>0.005 and not is_on_wall():
+		is_adjustin_pos = true
+		var direction = position.direction_to(start_position).x
+		motion.x = direction * SPEED * delta
+	else:
+		if is_adjustin_pos:
+			is_adjustin_pos = false
+			motion.x = 0
+	
+
+
 
 func ground_functions():
 		jump_released = false
