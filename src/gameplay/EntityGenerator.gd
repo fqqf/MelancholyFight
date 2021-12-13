5e75e9cd349a1cefd170b6b6979bfb6812e9b4d2
fqqf <77983090+fqqf@users.noreply.github.com>
2021-12-14 02:25:13 +0300

    Fix typos in entity gen

--- src/gameplay/EntityGenerator.gd
+++ src/gameplay/EntityGenerator.gd
@@ -17,21 +17,20 @@ var numus_struct_y
 var numus_size = 9
 var numus_gap = 0
 
+var platform
+
 func create_entities(chunk):	
-	for platform in chunk[0]:
+	for platform_ in chunk[0]: # Проверить память на платформе, кинуть кубик, если влезает - instance_numus()
+		self.platform = platform_
 		platform_memory = platform.memory
-		# Проверить память на платформе, кинуть кубик, если влезает - instance_numus()
-		numus_struct = [[0,0,0,0,0],[1,1,1,1,1],[0,0,0,0,1]]
-		numus_struct_x = 4234
-		numus_struct_y = 4525 # ЗАПОЛНИТЬ СВОИМ АЛГОРИТМОМ
 		
-		instance_numus_struct(platform, numus_struct)
+		numus_struct = [[0,0,0,0,0],[1,1,1,1,1],[0,0,0,0,1]]
+		instance_numus_struct(523542,42342342, numus_struct) # Вычислить x и y для numus_struct через старый код
 	return entities
 
 var numus_struct
 
-func instance_numus_struct(platform, numus_struct):
-	
+func instance_numus_struct(numus_struct_x, numus_struct_y, numus_struct):
 	var i = 0
 	var j = 0
 	for y_ in numus_struct: # Создать саму структуру
@@ -43,7 +42,6 @@ func instance_numus_struct(platform, numus_struct):
 				numus.x = numus_struct_x+i*numus_size+numus_gap*i
 				numus.y = numus_struct_y-j*numus_size-numus_gap*j
 			
-		
 				platform.add_entity(numus)
 				entities.add(numus)
 			i+=1
